fff61a2a975c374ba3c2ec8a4f9bb194070af39c
Eunwoo-1213 <127185136+Eunwoo-1213@users.noreply.github.com>
2023-04-11 15:35:58 +0900

    upload on vscode.dev

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,12 @@
-print('hello')
+#20231552 이은우
+grade = float(input('1학기 평균 평점은?'))  
+if 3.8 <=grade:
+    print('축하합니다! 장학금 지급 대상자입니다.')
+print('당신의 1학기 평균 평점 %.2f이다.' %(grade))
+
+#20231552 이은우
+n = int(input('정수 입력 >>'))
+if n%2 ==0:
+    print('%d은 짝수다.' % n)
+else:
+    print('%d은 홀수다.' % n)
